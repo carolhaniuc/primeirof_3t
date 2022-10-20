00c3bf33a5de144550748208eccc07ff4b29df50
carolhaniuc <108937084+carolhaniuc@users.noreply.github.com>
2022-10-20 11:10:53 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,28 +1,4 @@
 
-let tabuada = 2;
-function escreva(){
-    document.write("tabuada do " + tabuada + "<br>")
-    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-}  
-    
-    
-let lista = ["Jesus","Spessato","Jorge","Duarte","Waldete","Raquel"]
-function mostra(){
-    document.write(lista.legin + "<br>")
-    for(let i = 0; 1 < lista.length; 1++){
-        document.write(lista[1] + "<br>")
-    }
-}
-
 function mostratabuada(){
     for(let i = 1; i<= 10; i++){
         document.write("o valor do i" + i + "<br>");
@@ -41,4 +17,13 @@ function total(){
     document.write("Resultado: " + r);
 }
 
+function somaNota(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
 
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("result").innerHTML = "Soma: " + r;
+}
